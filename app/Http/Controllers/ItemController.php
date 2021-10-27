c18efd4706f975eb5a21a6f001addd07aaa92653
heinerbehrends <heinerbehrends@hotmail.com>
2021-10-27 21:43:28 +0200

    Added Item Controller with CRUD methods

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response returns the newly created item as json
+     */
+    public function store(Request $request)
+    {   
+        $input = $request->input();
+        Item::create($input);
+        
+        return Item::latest()->first();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Item $item)
+    {
+        $input = $request->input();
+        $item->update($input);
+
+        return $item;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Item $item)
+    {
+        $item->delete();
+    }
+}
